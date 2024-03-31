0da38cb6927a5f42aa6241ffebb9da95d90807a7
WeinbergerPeti <weinbergerpeti@gmail.com>
2024-03-31 21:42:46 +0200

    alap kinézet

---
+++ script.js
@@ -0,0 +1,26 @@
+$(function()
+{
+    console.log("csecs");
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var beer = document.querySelector('.beer');
+  
+    // Amikor a linkre kattintasz
+    document.querySelector('a').addEventListener('click', function(event) {
+      event.preventDefault(); // Megakadályozza az alapértelmezett navigációt
+  
+      // Sör feltöltése
+      beer.classList.add('full');
+      setTimeout(function() {
+        beer.classList.add('top');
+        // 5 másodperc után az oldal kiürítése
+        setTimeout(function() {
+            beer.classList.remove('top');
+            beer.classList.remove('full');
+          window.location.href = "masik.html"; // Az új oldal URL-jére navigál
+        }, 5000);
+      }, 500);
+    });
+  });
+  
